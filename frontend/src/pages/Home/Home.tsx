b0022869499c3bf8ebf69f31848f6f9dba452fa4
LucasNeevs <lucasneves.dev@gmail.com>
2021-05-07 14:18:58 -0300

    Home, navigation

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,21 @@
-import ImgDsDark from 'assets/img/ds_dark.svg';
+import NavBar from 'components/NavBar/NavBar';
+import Footer from 'components/Footer/Footer';
+import { Link } from 'react-router-dom';
 
 const Home = (): JSX.Element => (
-  <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-    <h1>Home</h1>
-  </div>
+  <>
+    <NavBar />
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4">DSVendas</h1>
+        <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+        <hr />
+        <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+        <Link className="btn btn-primary btn-lg" to="/dashboard" style={{ marginTop: 20 }}>Dashboard</Link>
+      </div>
+    </div>
+    <Footer />
+  </>
 );
 
 Home.displayName = 'Home';
